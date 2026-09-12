b9cd0f1253bbb215986f0c6317e67db598bf695f
Svetlana Krulik <box15@rambler.ru>
2026-09-12 21:00:39 +0300

    Выполнен п. 1.3.6

--- 134-led-module/main.c
+++ 134-led-module/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "led.h"
+#include "log.h"
 
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
@@ -22,16 +23,20 @@ void handle_command(int command)
     if (command == 'e')
     {
         led_set(true);
-		printf("led %s\n", led_is_on() ? "on" : "off");
+		LOG_INF("led %s\n", led_is_on() ? "on" : "off");
     }
     else if (command == 'd')
     {
         led_set(false);
-		printf("led %s\n", led_is_on() ? "on" : "off");
+		LOG_INF("led %s\n", led_is_on() ? "on" : "off");
+	}
+	else if (command == 'v')
+    {
+        log_version();
     }
     else
     {
-        printf("unknown command: %c\n", command);
+        LOG_ERR("unknown command: %c\n", command);
     }
 
 }
@@ -54,7 +59,7 @@ int main()
         if (previous == true && current == false)
         {
             led_toggle();
-			printf("led %s\n", led_is_on() ? "on" : "off");
+			LOG_INF("led %s\n", led_is_on() ? "on" : "off");
         }
 
         previous = current;
@@ -66,7 +71,7 @@ int main()
         {
             continue;
         }
-
+		LOG_DBG("got %c\n", command);
         handle_command(command);	
 		
 		
